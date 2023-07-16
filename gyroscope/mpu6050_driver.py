cf7edb1befc2f68d772aeacb011096c0d1d9b839
Kesler Mathieu <kmat5@icloud.com>
2023-07-15 22:59:31 -0400

    altering main()

--- gyroscope/mpu6050_driver.py
+++ gyroscope/mpu6050_driver.py
@@ -19,30 +19,30 @@ class GyroscopeDriver():
         return np.rad2deg(self.__poll_sensor(0))
     
     # orientation in Y-Axis
-    def read_yaw(self):
+    def read_pitch(self):
         return np.rad2deg(self.__poll_sensor(1))
 
     # orientation in Z-Axis
-    def read_pitch(self):
+    def read_yaw(self):
         return np.rad2deg(self.__poll_sensor(2))
 
 
 def main():
-    # g_driver = GyroscopeDriver()
-    # while True:
-    #     print("X: " + str(g_driver.read_roll()))
-    #     print("Y: " + str(g_driver.read_yaw()))
-    #     print("Z: " + str(g_driver.read_pitch()))
-    #     print(" ")
-    #     time.sleep(1)
-    
-    i2c = board.I2C()  # uses board.SCL and board.SDA
-    mpu = adafruit_mpu6050.MPU6050(i2c)
-
+    g_driver = GyroscopeDriver()
     while True:
-        print("Gyro X:{x} Y:{y}, Z:{z} deg/s".format(x = np.rad2deg(mpu.gyro[0]), y = np.rad2deg(mpu.gyro[1]), z = np.rad2deg(mpu.gyro[2])))
-        print("")
+        #print("X: " + str(g_driver.read_roll()))
+        print("Y: " + str(g_driver.read_yaw()))
+        #print("Z: " + str(g_driver.read_pitch()))
+        print(" ")
         time.sleep(1)
+    
+    # i2c = board.I2C()  # uses board.SCL and board.SDA
+    # mpu = adafruit_mpu6050.MPU6050(i2c)
+
+    # while True:
+    #     print("Gyro X:{x} Y:{y}, Z:{z} deg/s".format(x = np.rad2deg(mpu.gyro[0]), y = np.rad2deg(mpu.gyro[1]), z = np.rad2deg(mpu.gyro[2])))
+    #     print("")
+    #     time.sleep(1)
 
 if __name__ == '__main__':
     main()
