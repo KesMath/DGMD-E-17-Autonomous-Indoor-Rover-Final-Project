e6b25e3d9c2bc2db20d68c4e8dd6ec2326e29e54
Kesler Mathieu <kmat5@icloud.com>
2023-08-18 21:48:10 -0400

    alter code to test for behavioral change on hardware

--- motor_driver.py
+++ motor_driver.py
@@ -181,7 +181,9 @@ async def main():
     with ProcessPool() as pool:
         print("executing processes...")
         p1 = pool.schedule(test_fn1)
-        p2 = pool.schedule(spin_left_90_degrees, roverBase)
+        p2 = pool.schedule(test_fn2)
+        #p1 = pool.schedule(gyroscope_driver.poll_sensor_until_orthogonally_left)
+        #p2 = pool.schedule(spin_left_90_degrees, roverBase)
 
         # when process A finishes (i.e. when rover turns 90deg,) terminate process B (i.e. stop motors from spinning)
         #executor will automatically shutdown when control flow exits context manager
