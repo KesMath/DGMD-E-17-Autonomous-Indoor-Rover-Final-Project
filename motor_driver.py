78bdea68080589e29aaadea706e34151b7aeb824
Kesler Mathieu <kmat5@icloud.com>
2023-08-18 23:27:00 -0400

    testing

--- motor_driver.py
+++ motor_driver.py
@@ -175,6 +175,8 @@ def test_fn2():
 async def main():
     robot_client = await connect()
     roverBase = Base.from_robot(robot_client, 'viam_base')
+    await spin_left_90_degrees(roverBase)
+    gyroscope_driver.poll_sensor_until_orthogonally_left()
     ########################## TESTING WITH ProcessPool() ##########################
 
     # Dispatch 2 processes - Process A for Sensor Polling, Process B for motor spinning
