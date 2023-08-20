976d2e23a4a4884a75fa24b319214d98a65b09af
Kesler Mathieu <kmat5@icloud.com>
2023-08-19 22:39:59 -0400

    let's test again

--- motor_driver.py
+++ motor_driver.py
@@ -183,7 +183,7 @@ async def main():
     ### TECHNIQUE 1
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(test_fn1())
+        loop.run_until_complete(gyroscope_driver.poll_sensor_until_orthogonally_left())
         #await spin_left_90_degrees(roverBase)
     finally:
         print("closing event loop...")
