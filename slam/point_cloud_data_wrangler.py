f98fd47c85d2bdf699897b2bc1c60fd480909a8c
Kesler Mathieu <kmat5@icloud.com>
2023-10-02 22:21:12 -0400

    add data wrangling steps

--- slam/point_cloud_data_wrangler.py
+++ slam/point_cloud_data_wrangler.py
@@ -1,6 +1,9 @@
 # Data Wrangling Steps for PyntCloud Parser to load in dataset correctly:
 # (1) strip 'point_cloud_pcd_chunk: "' in heading and trailing '"'
-# (2) remove '\n'
+# (2) remove '\n' with actual line break
+# https://stackoverflow.com/questions/42965689/replacing-a-text-with-n-in-it-with-a-real-n-output
+# https://stackoverflow.com/questions/54586164/how-to-replace-all-instances-of-n-in-a-string-with-linebreaks
+
 def convert_dataset_to_pcd_file_format(str):
     pass
 
