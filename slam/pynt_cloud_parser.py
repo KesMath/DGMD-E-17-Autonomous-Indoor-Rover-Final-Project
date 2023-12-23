084dbe69fcf0bda3cdcfefde3dcb726169a46c05
Kesler Mathieu <kmat5@icloud.com>
2023-12-22 22:10:23 -0500

    adding point cloud snapshots

--- slam/pynt_cloud_parser.py
+++ slam/pynt_cloud_parser.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    cloud = PyntCloud.from_file("slam/map1.pcd")
+    cloud = PyntCloud.from_file("slam/map_filled.pcd")
     x_arr = cloud.points.x
     y_arr = cloud.points.y
-
+    #print(x_arr)
+    #print(y_arr)
     plt.scatter(x_arr, y_arr)
     plt.xlabel('Horizontal Distance') 
     plt.ylabel('Vertical Distance')
